1589e7e31074b9e1a67c6d42e96cb628e416eb6d
ovo-tim <ovo-tim@qq.com>
2023-01-12 22:47:58 +0800

    增加一些空壳函数，修改example.py使用继承

--- TinyMCE_on_Pyqt.py
+++ TinyMCE_on_Pyqt.py
@@ -5,16 +5,27 @@ from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 from PySide6.QtWebEngineWidgets import *
 
-
 class TinyMCE_on_PyQt_window(QWidget):
-    def __init__(self,port=1145):
+    def __init__(self):
         super(TinyMCE_on_PyQt_window, self).__init__()
 
-        self.service_port=port
-        self.html_path="./html"
+        # 创建浏览器控件
+        self.html_path="."
         self.html_file="/main.html"
-
         self.browser=QWebEngineView()
         self.url=self.html_path+self.html_file
-        self.browser.load(QUrl(self.url))
+        # print(self.url)
+        self.browser.load(QUrl("http://baidu.com"))
         
+        # 设置QWidget的layout
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.browser)
+        self.setLayout(self.layout)
+
+        
+    def get_file(self):
+        pass
+        return
+
+    def save_file(self):
+        pass
--- example.py
+++ example.py
@@ -4,9 +4,28 @@ from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 import sys
 
+from qt_material import apply_stylesheet
+
 import TinyMCE_on_Pyqt as editor
 
+class my_TinyMCE(editor.TinyMCE_on_PyQt_window):
+    def __init__(self):
+        super(my_TinyMCE, self).__init__()
+        # 添加保存按钮
+        self.save_button = QPushButton("保存文件",self)
+        self.save_button.clicked.connect(self.save_file)
+        self.layout.addWidget(self.save_button)
+
+    # 监听键盘
+    def keyPressEvent(self, event):
+        if QApplication.keyboardModifiers() == Qt.ControlModifier and event.key() == Qt.Key_S: #检测CtrlS保存按键
+            self.save_file()
+            
+
+
+
 app=QApplication(sys.argv)
-win=editor.TinyMCE_on_PyQt_window()
+apply_stylesheet(app, theme='dark_teal.xml')
+win=my_TinyMCE()
 win.show()
 app.exit(app.exec())
